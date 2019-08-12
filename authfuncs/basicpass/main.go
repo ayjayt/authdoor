0d0eb8db2fb50ea534d0e94629e159cf54150801
Andrew Pikul <ajpikul@gmail.com>
2019-08-12 23:49:23 +0000

    Dump form values via fmt, not encode

--- authfuncs/basicpass/main.go
+++ authfuncs/basicpass/main.go
@@ -76,7 +76,7 @@ func (b *BasicPass) Check(w http.ResponseWriter, r *http.Request) (authdoor.Auth
 	}
 	if r.Method == "POST" {
 		r.ParseForm()
-		fmt.Printf("Form: %v\n", r.Form.Encode())
+		fmt.Printf("Form: %+v\n", r.Form)
 		// if correct
 		sess := uuid.New().String()
 		http.SetCookie(w, &http.Cookie{
