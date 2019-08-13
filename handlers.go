34dfe0ff27e0372b4395f3839555965c3a36fd79
Andrew Pikul <ajpikul@gmail.com>
2019-08-13 21:36:21 +0000

    Add reverse proxy parser to handlers

--- handlers.go
+++ handlers.go
@@ -21,3 +21,17 @@ func (rsh *redirectSchemeHandler) ServeHTTP(w http.ResponseWriter, r *http.Reque
 func RedirectSchemeHandler(scheme string, code int) http.Handler {
 	return &redirectSchemeHandler{scheme, code}
 }
+
+type ReverseProxy struct {
+	http.Handler
+}
+
+func NewSingleHostReverseProxy(target string) (*ReverseProxy, error) {
+	targetURL, err := url.Parse(targetURL)
+	if err != nil {
+		return nil, err
+	}
+	return &ReverseProxy{
+		Handler: httputil.NewSingleHostReverseProxy(targetURL),
+	}, nil
+}
