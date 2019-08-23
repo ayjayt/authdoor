fc773f77f08ca92de28c6d2597b6d462a348fd25
Andrew Pikul <ajpikul@gmail.com>
2019-08-23 21:13:48 +0000

    Properly comment handlers

--- handlers.go
+++ handlers.go
@@ -6,11 +6,13 @@ import (
 	"net/url"
 )
 
+// redirectSchemeHandler holds the new scheme (usually HTTPS) and redirect code (30x) to use during redirection
 type redirectSchemeHandler struct {
 	scheme string
 	code   int
 }
 
+// ServeHTTP rewrites the requests URL and appropriately and then calls Redirect.
 func (rsh *redirectSchemeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	newURL := r.URL
 	if r.URL.IsAbs() == false {
@@ -20,14 +22,17 @@ func (rsh *redirectSchemeHandler) ServeHTTP(w http.ResponseWriter, r *http.Reque
 	http.Redirect(w, r, newURL.String(), rsh.code)
 }
 
+// RedirectSchemeHandler returns a new http.Handler
 func RedirectSchemeHandler(scheme string, code int) http.Handler {
 	return &redirectSchemeHandler{scheme, code}
 }
 
+// ReverseProxy is a wrapper for httputil's reverse proxy that just does the tedious working of parsing a url string. Paths are always passed to the proxy, it's just the host that's rewritten. I'm not sure if the proxy receives the original host.
 type ReverseProxy struct {
 	http.Handler
 }
 
+// NewSingleHostReverseProxy is the constructor for the ReverseProxy struct that actually does the work
 func NewSingleHostReverseProxy(target string) (*ReverseProxy, error) {
 	targetURL, err := url.Parse(target)
 	if err != nil {
--- handlers_test.go
+++ handlers_test.go
@@ -33,3 +33,9 @@ func TestRedirectSchemeHandler(t *testing.T) {
 	require.Equal(t, []string{"https://ajpikul.com/thing"}, responseRecorder2.Header()[http.CanonicalHeaderKey("location")])
 	t.Logf("New URL: %+v", responseRecorder2.Header())
 }
+
+// lets start a local server that just dumps the request and returns okay
+// lets create a handler that just dumps the request
+// lets start another local server that reverse proxies to the first one and also redirects to it
+// if we redirect, because we're using my redirect, i know we're just changing the scheme
+// if we use the reverse proxy, do we just change the host?
